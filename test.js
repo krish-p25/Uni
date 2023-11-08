ddbb840dace2e84eae81ae2b5a974d27fa0586de
KP <80683788+krish-p25@users.noreply.github.com>
2023-11-08 03:20:38 +0000

    a formatted csv files to remove missing information

--- test.js
+++ test.js
@@ -1,89 +1,154 @@
 const fs = require('fs');
 const path = require('path');
-const sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('simulator', 'postgres', 'Wperkin-10', {
+    host: 'localhost',
+    port: '5432',
+    dialect: 'postgres',
+    logging: false,
+  });
 
 const scenario_one = sequelize.define('scenario_one', {
     utc: {
-        type: sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false
     },
     time: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     position_x: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     position_y: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     position_z: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     rotation_x: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     rotation_y: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     rotation_z: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     velocity_x: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     velocity_y: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     velocity_z: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     throttle: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     brake: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     steering: {
-        type: sequelize.NUMERIC,
+        type: DataTypes.NUMERIC,
         allowNull: false
     },
     car_spawned: {
-        type: sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         allowNull: false
     },
     ped_spawned: {
-        type: sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         allowNull: false
     },
     car_collide: {
-        type: sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         allowNull: false
     },
     ped_collide: {
-        type: sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         allowNull: false
-    }
+    },
+    id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+    },
 }, {
-    timestamps: false
+    timestamps: false,
+    tableName: 'scenario_one',
+    freezeTableName: true
 });
 
 async function import_scenario_one_from_csv() {
-    for (let counter = 1; counter < 57; counter++) {
-        const data_csv = fs.readFileSync(`./Data/Scenario1n/S_1_${counter}.csv`)
-        console.log(data_csv.toString())
-        break
+    for (let counter = 3; counter < 57; counter++) {
+        const data_csv = fs.readFileSync(`./Data/Scenario1n/S_1_${counter}.csv`).toString().split('\n');
+        data_csv.shift();
+        const id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        for (const row of data_csv) {
+            const row_data = row.split(',');
+            const utc = row_data[0];
+            if (!(!!(row_data[0]))) continue;
+            const time = row_data[1].trim();
+            const position_x = row_data[2].trim();
+            const position_y = row_data[3].trim();
+            const position_z = row_data[4].trim();
+            const rotation_x = row_data[5].trim();
+            const rotation_y = row_data[6].trim();
+            const rotation_z = row_data[7].trim();
+            const velocity_x = row_data[8].trim();
+            const velocity_y = row_data[9].trim();
+            const velocity_z = row_data[10].trim();
+            const throttle = row_data[11].trim();
+            const brake = row_data[12].trim();
+            const steering = row_data[13].trim();
+            const car_spawned = row_data[14].trim() === 'True' ? true : false;
+            const ped_spawned = row_data[15].trim() === 'True' ? true : false;
+            const ped_collide = parseFloat(row_data[16].trim()) > 0 ? true : false;
+            const car_collide = row_data[17].trim() === 'True' ? true : false;
+
+            try {
+                await scenario_one.create({
+                    utc,
+                    time,
+                    position_x,
+                    position_y,
+                    position_z,
+                    rotation_x,
+                    rotation_y,
+                    rotation_z,
+                    velocity_x,
+                    velocity_y,
+                    velocity_z,
+                    throttle,
+                    brake,
+                    steering,
+                    car_spawned,
+                    ped_spawned,
+                    ped_collide,
+                    car_collide,
+                    id
+                });
+            }
+            catch (err) {
+                console.log(`${counter} - ${err}`);
+            }
+
+        }
+        console.log('inserted full csv file', counter)
     }
 }
 import_scenario_one_from_csv()
