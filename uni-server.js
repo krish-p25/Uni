f960fd48ff4162beb2fb2c365dce86652da09b3e
KP <80683788+krish-p25@users.noreply.github.com>
2023-11-08 04:36:33 +0000

    configured api router

--- uni-server.js
+++ uni-server.js
@@ -5,6 +5,7 @@ const path = require('path')
 const fs = require('fs')
 const bodyParser = require('body-parser');
 const https = require('https');
+const api = require('./api.js');
 const cert = fs.readFileSync('./certs/certificate.crt');
 const ca = fs.readFileSync('./certs/ca_bundle.crt');
 const key = fs.readFileSync('./certs/private.key');
@@ -14,7 +15,6 @@ let options = {
     ca: ca,
     key: key
 };
-
 
 https.createServer(options, app).listen(443, () => {
     console.log(new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' }), 'HTTPS Server running on port 443');
@@ -49,3 +49,10 @@ app.get('/simulator/dashboard.js', (req, res) => {
 app.listen(80, () => {
     console.log(new Date().toLocaleString('en-GB', { timeZone: 'Europe/London' }), 'HTTP Server running on port 80');
 });
+
+app.get('/overview/scenario-one', (req, res) => {
+    res.sendFile(__dirname + '/overview/scenario-one.html');
+})
+
+//API router
+app.use('/api', api);
